475cfd25c6e36b14295193ffa40d6143cde6b90b
William Yallop <hello@williamyallop.com>
2024-01-27 10:12:33 +0000

    Fixed tsup config with asset json on import and started planning form markup and attributes

--- packages/toggler/tsup.config.js
+++ packages/toggler/tsup.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from "tsup";
-import tsupConfig from "@repo/config/tsupconfig-base.json";
+import tsupConfig from "@repo/config/tsupconfig-base.json" assert {
+	type: "json",
+};
 
 export default defineConfig(tsupConfig);
